87216723dec59f4b8f5d15066d19d2f354170885
LevkovychBorya <38095999+LevkovychBorya@users.noreply.github.com>
2020-04-27 15:40:57 +0300

    Bug Fixed

--- loadbalancer/loadbalancer-entrypoint.sh
+++ loadbalancer/loadbalancer-entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -e
-echo "upstream website {"                                             >> /etc/nginx/sites-available/default
-echo "  server "$IP":81;"                                             >> /etc/nginx/sites-available/default
-echo "  server "$IP":82;"                                             >> /etc/nginx/sites-available/default
-echo "  server "$IP":83;"                                             >> /etc/nginx/sites-available/default
-echo "}"                                                              >> /etc/nginx/sites-available/default
-echo "server {"                                                       >> /etc/nginx/sites-available/default
-echo "  listen 80;"                                                   >> /etc/nginx/sites-available/default
-echo "  location / {"                                                 >> /etc/nginx/sites-available/default
-echo '  proxy_set_header Host \$host;'                                >> /etc/nginx/sites-available/default
-echo '  add_header Cache-Control "no-cache";'                         >> /etc/nginx/sites-available/default
-echo "  proxy_pass http://website;"                                   >> /etc/nginx/sites-available/default
-echo "  }"                                                            >> /etc/nginx/sites-available/default
-echo "}"                                                              >> /etc/nginx/sites-available/default
+echo "upstream website {"                     >> /etc/nginx/sites-available/default
+echo "  server "$IP":81;"                     >> /etc/nginx/sites-available/default
+echo "  server "$IP":82;"                     >> /etc/nginx/sites-available/default
+echo "  server "$IP":83;"                     >> /etc/nginx/sites-available/default
+echo "}"                                      >> /etc/nginx/sites-available/default
+echo "server {"                               >> /etc/nginx/sites-available/default
+echo "  listen 80;"                           >> /etc/nginx/sites-available/default
+echo '  add_header Cache-Control "no-store";' >> /etc/nginx/sites-available/default
+echo "  location / {"                         >> /etc/nginx/sites-available/default
+echo "    proxy_pass http://website;"         >> /etc/nginx/sites-available/default
+echo "    proxy_set_header Host \$host;"      >> /etc/nginx/sites-available/default
+echo "  }"                                    >> /etc/nginx/sites-available/default
+echo "}"                                      >> /etc/nginx/sites-available/default
 exec "$@"
