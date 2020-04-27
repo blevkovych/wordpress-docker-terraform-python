9ac970fe1d3a31b1609818dd51dc8aed693a81ae
LevkovychBorya <38095999+LevkovychBorya@users.noreply.github.com>
2020-04-27 13:27:19 +0300

    CSS wasn't loading

--- loadbalancer/loadbalancer-entrypoint.sh
+++ loadbalancer/loadbalancer-entrypoint.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 set -e
-echo "upstream website {"                     >> /etc/nginx/sites-available/default
-echo "  server "$IP":81;"                     >> /etc/nginx/sites-available/default
-echo "  server "$IP":82;"                     >> /etc/nginx/sites-available/default
-echo "  server "$IP":83;"                     >> /etc/nginx/sites-available/default
-echo "}"                                      >> /etc/nginx/sites-available/default
-echo "server {"                               >> /etc/nginx/sites-available/default
-echo "  listen 80;"                           >> /etc/nginx/sites-available/default
-echo '  add_header Cache-Control "no-store";' >> /etc/nginx/sites-available/default
-echo "  location / {"                         >> /etc/nginx/sites-available/default
-echo "    proxy_pass http://website;"         >> /etc/nginx/sites-available/default
-echo "    proxy_set_header Host \$host;"      >> /etc/nginx/sites-available/default
-echo "  }"                                    >> /etc/nginx/sites-available/default
-echo "}"                                      >> /etc/nginx/sites-available/default
+echo "upstream website {"                                             >> /etc/nginx/sites-available/default
+echo "  server "$IP":81;"                                             >> /etc/nginx/sites-available/default
+echo "  server "$IP":82;"                                             >> /etc/nginx/sites-available/default
+echo "  server "$IP":83;"                                             >> /etc/nginx/sites-available/default
+echo "}"                                                              >> /etc/nginx/sites-available/default
+echo "server {"                                                       >> /etc/nginx/sites-available/default
+echo "  listen 80;"                                                   >> /etc/nginx/sites-available/default
+echo "  location / {"                                                 >> /etc/nginx/sites-available/default
+echo '  proxy_set_header Host $http_host;'                            >> /etc/nginx/sites-available/default
+echo '  proxy_set_header X-Real-IP $remote_addr;'                     >> /etc/nginx/sites-available/default
+echo '  proxy_set_header X-Scheme $scheme;'                           >> /etc/nginx/sites-available/default
+echo '  proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;' >> /etc/nginx/sites-available/default
+echo '  proxy_set_header X-Forwarded-Proto $scheme;'                  >> /etc/nginx/sites-available/default
+echo '  proxy_redirect    off;'                                       >> /etc/nginx/sites-available/default
+echo '  add_header Pragma "no-cache";'                                >> /etc/nginx/sites-available/default
+echo '  add_header Cache-Control "no-cache";'                         >> /etc/nginx/sites-available/default
+echo "  proxy_pass http://website;"                                   >> /etc/nginx/sites-available/default
+echo "  sub_filter 'action=\"/'  'action=\"/';"                       >> /etc/nginx/sites-available/default
+echo "  sub_filter 'href=\"/'  'href=\"/';"                           >> /etc/nginx/sites-available/default
+echo "  sub_filter 'src=\"/'  'src=\"/'"                              >> /etc/nginx/sites-available/default
+echo "  sub_filter_once off;"                                         >> /etc/nginx/sites-available/default
+echo "  }"                                                            >> /etc/nginx/sites-available/default
+echo "}"                                                              >> /etc/nginx/sites-available/default
 exec "$@"
