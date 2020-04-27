44ab5286844e9064c50afcc65d355fb529cd829d
LevkovychBorya <38095999+LevkovychBorya@users.noreply.github.com>
2020-04-27 15:23:24 +0300

    Update loadbalancer-entrypoint.sh

--- loadbalancer/loadbalancer-entrypoint.sh
+++ loadbalancer/loadbalancer-entrypoint.sh
@@ -8,19 +8,19 @@ echo "}"                                                              >> /etc/ng
 echo "server {"                                                       >> /etc/nginx/sites-available/default
 echo "  listen 80;"                                                   >> /etc/nginx/sites-available/default
 echo "  location / {"                                                 >> /etc/nginx/sites-available/default
-echo '  proxy_set_header Host $http_host;'                            >> /etc/nginx/sites-available/default
-echo '  proxy_set_header X-Real-IP $remote_addr;'                     >> /etc/nginx/sites-available/default
-echo '  proxy_set_header X-Scheme $scheme;'                           >> /etc/nginx/sites-available/default
-echo '  proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;' >> /etc/nginx/sites-available/default
-echo '  proxy_set_header X-Forwarded-Proto $scheme;'                  >> /etc/nginx/sites-available/default
+echo '  proxy_set_header Host \$http_host;'                            >> /etc/nginx/sites-available/default
+echo '  proxy_set_header X-Real-IP \$remote_addr;'                     >> /etc/nginx/sites-available/default
+echo '  proxy_set_header X-Scheme \$scheme;'                           >> /etc/nginx/sites-available/default
+echo '  proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;' >> /etc/nginx/sites-available/default
+echo '  proxy_set_header X-Forwarded-Proto \$scheme;'                  >> /etc/nginx/sites-available/default
 echo '  proxy_redirect    off;'                                       >> /etc/nginx/sites-available/default
 echo '  add_header Pragma "no-cache";'                                >> /etc/nginx/sites-available/default
 echo '  add_header Cache-Control "no-cache";'                         >> /etc/nginx/sites-available/default
 echo "  proxy_pass http://website;"                                   >> /etc/nginx/sites-available/default
-#echo "  sub_filter 'action=\"/'  'action=\"/';"                       >> /etc/nginx/sites-available/default
-#echo "  sub_filter 'href=\"/'  'href=\"/';"                           >> /etc/nginx/sites-available/default
-#echo "  sub_filter 'src=\"/'  'src=\"/'"                              >> /etc/nginx/sites-available/default
-#echo "  sub_filter_once off;"                                         >> /etc/nginx/sites-available/default
+echo "  sub_filter 'action=\"/'  'action=\"/';"                       >> /etc/nginx/sites-available/default
+echo "  sub_filter 'href=\"/'  'href=\"/';"                           >> /etc/nginx/sites-available/default
+echo "  sub_filter 'src=\"/'  'src=\"/'"                              >> /etc/nginx/sites-available/default
+echo "  sub_filter_once off;"                                         >> /etc/nginx/sites-available/default
 echo "  }"                                                            >> /etc/nginx/sites-available/default
 echo "}"                                                              >> /etc/nginx/sites-available/default
 exec "$@"
